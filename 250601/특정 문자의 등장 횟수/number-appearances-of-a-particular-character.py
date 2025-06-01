c091c8c489a07c6fbab136eea3e6962a728fd974
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-01 11:15:37 +0900

    Title: 특정 문자의 등장 횟수, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250601/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
+++ 250601/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
@@ -1,4 +1,8 @@
 arr = ['ee', 'eb']
 s = input()
 for e in arr:
-    print(s.count(e), end=' ')
+    cnt = 0    
+    for i in range(len(s) - len(e) + 1):
+        if s[i] == e[0] and s[i+1] == e[1]:
+            cnt += 1
+    print(cnt, end=' ')
