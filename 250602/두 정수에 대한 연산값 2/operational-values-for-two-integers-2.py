af988483cf1e19b1a417284aaa62eee4c2679f24
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-02 16:43:34 +0900

    Title: 두 정수에 대한 연산값 2, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250602/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
@@ -0,0 +1,13 @@
+a, b = map(int, input().split())
+
+def modify(a, b):
+    if a < b:
+        a += 10
+        b *= 2
+    else:
+        a *= 2
+        b += 10
+    return a, b
+
+a, b = modify(a, b)
+print(a, b)
