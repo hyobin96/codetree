0e4b650515461d51fa29a5ab6dc4a5a98b1b96d2
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-02 12:15:57 +0900

    Title: 정수만 추출하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250602/정수만 추출하기/extract-only-integers.py
+++ 250602/정수만 추출하기/extract-only-integers.py
@@ -1,7 +1,10 @@
 arr = input().split()
 total = 0
 for e in arr:
+    idx = len(e)
     for i in range(len(e)):
         if not e[i].isdigit():
-            total += int(e[:i])
+            idx = i
+            break
+    total += int(e[:idx])
 print(total)
