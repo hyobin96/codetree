6df2e77e3d0ba3d7bead29a3f0d7ce08c90a10f7
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-02 16:08:01 +0900

    Title: 2021년 날짜의 유무, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250602/2021년 날짜의 유무/with-or-without-2021.py
+++ 250602/2021년 날짜의 유무/with-or-without-2021.py
@@ -5,6 +5,9 @@ def is_exist(M, D):
         return False
     if M == 2 and D > 28:
         return False
+    if not(M == 1 or M == 3 or M == 5 or M == 7 or M == 8 or M == 10 or M == 12):
+        if D > 30:
+            return False
     return True
 
 if is_exist(M, D):
