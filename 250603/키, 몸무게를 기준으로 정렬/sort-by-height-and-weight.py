2d7f6a84bb820d7e940759bb9d7751c7e6d9988e
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-03 16:51:51 +0900

    Title: 키, 몸무게를 기준으로 정렬, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250603/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.py
+++ 250603/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.py
@@ -1,5 +1,5 @@
 n = int(input())
-arr = [tuple(input().split()) for _ in range(5)]
+arr = [tuple(input().split()) for _ in range(n)]
 arr.sort(key=lambda x: (int(x[1]), -int(x[2])))
 for e in arr:
     print(e[0], e[1], e[2])
