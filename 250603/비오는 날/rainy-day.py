4305659a3e9b3940e6fcca36d6f81bfb002d039a
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-03 14:41:41 +0900

    Title: 비오는 날, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250603/비오는 날/rainy-day.py
+++ 250603/비오는 날/rainy-day.py
@@ -4,29 +4,14 @@ class Data:
         self.y = yo
         self.n = nal
 
-def is_last(d1, d2):
-    d1 = tuple(map(int, d1.split('-')))
-    d2 = tuple(map(int, d2.split('-')))
-    for x1, x2 in zip(d1, d2):
-        if x1 < x2:
-            return True
-        elif x1 > x2:
-            return False
-    return True
-
 n = int(input())
 arr = [tuple(input().split()) for _ in range(n)]
 datas = [Data(d, y, n) for d, y, n in arr]
 
-lat_idx = 0
-for i, d in enumerate(datas):
-    if d.n == 'Rain':
-        lat_idx = i
-        break
+ans = Data('9999-99-99', '', '')
 
-for i in range(lat_idx + 1, len(datas)):
-    if datas[i].n == 'Rain' and is_last(datas[i].d, datas[lat_idx].d):
-        lat_idx = i
+for i in range(len(datas)):
+    if datas[i].n == 'Rain' and datas[i].d < ans.d:
+        ans = datas[i]
 
-last_d = datas[lat_idx]
-print(last_d.d, last_d.y, last_d.n)
+print(ans.d, ans.y, ans.n)
