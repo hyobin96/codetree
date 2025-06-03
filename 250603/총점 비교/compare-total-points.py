dae1e21a507f610c5d4292acf4356f140d1b3280
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-03 16:33:21 +0900

    Title: 총점 비교, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250603/총점 비교/compare-total-points.py
+++ 250603/총점 비교/compare-total-points.py
@@ -1,5 +1,5 @@
 n = int(input())
 arr = [tuple(input().split()) for _ in range(n)]
-arr.sort(key=lambda x: x[1] + x[2] + x[3])
+arr.sort(key=lambda x: int(x[1]) + int(x[2]) + int(x[3]))
 for e in arr:
     print(e[0], e[1], e[2], e[3])
