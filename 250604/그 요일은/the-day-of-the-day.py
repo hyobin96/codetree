108e6fa04f141b42c59061132cfbe22b39b776d3
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-04 14:47:26 +0900

    Title: 그 요일은, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250604/그 요일은/the-day-of-the-day.py
+++ 250604/그 요일은/the-day-of-the-day.py
@@ -1,4 +1,3 @@
-import math
 m1, d1, m2, d2 = map(int, input().split())
 a = input()
 days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
@@ -7,4 +6,4 @@ pos = yoil.index(a)
 def cal():
     return sum(days[:m2]) + d2 - (sum(days[:m1]) + d1 + pos) + 1
 
-print(math.ceil(cal() / 7))
+print(cal() // 7 + 1)
