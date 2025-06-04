38892854ec3a571f24759babbf8e9d5f5a9f0bfc
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-04 18:25:39 +0900

    Title: 왔다 갔던 구역 2, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250604/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,24 @@
+n = int(input())
+arr = [0] * (2001)
+start = 1000
+
+def move(x, c):
+    global start
+    if c == 'L':
+        for i in range(start - 1, start-x-1, -1):
+            arr[i] += 1
+        start -= x
+    else:
+        for i in range(start + 1, start + x + 1):
+            arr[i] += 1
+        start += x
+
+for _ in range(n):
+    x, c = input().split()
+    move(int(x), c)
+
+total = 0
+for e in arr:
+    if e >= 2:
+        total += 1
+print(total)
