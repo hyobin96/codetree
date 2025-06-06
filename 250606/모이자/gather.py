9b6b334287facb5491b3c8a58456a2ec6f1d252d
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-06 15:58:08 +0900

    Title: 모이자, Time: 45ms, Memory: 17MB, Status: Passed - Codetree

--- 250606/모이자/gather.py
+++ 250606/모이자/gather.py
@@ -4,7 +4,7 @@ dist = 0
 for i in range(n):
     dist += i * arr[i]
 
-_min = 100 * 100
+_min = 100 * 100 * 100
 for i in range(1, n):
     dist += sum(arr[:i]) - sum(arr[i:]) 
     _min = min(_min, dist)
