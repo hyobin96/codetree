5e9e5e5dac757e92d5c4f9791c93627c59d73892
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-07 00:43:09 +0900

    Title: 이상한 진수 2, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 250607/이상한 진수 2/awkward-digits-2.py
+++ 250607/이상한 진수 2/awkward-digits-2.py
@@ -1,8 +1,8 @@
 n = input()
 num = int(n, 2)
 _max = 0
-for i in range(1, len(n)):
-    if n[i] == 1:
+for i in range(0, len(n)):
+    if n[i] == '1':
         tmp = num - 2 ** (len(n) - 1 - i)
     else:
         tmp = num + 2 ** (len(n) - 1 - i)
