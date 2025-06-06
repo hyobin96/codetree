9cd4926009434d407c0208a836f8c7dfacd7a88c
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-07 00:40:23 +0900

    Title: 이상한 진수 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250607/이상한 진수 2/awkward-digits-2.py
@@ -0,0 +1,10 @@
+n = input()
+num = int(n, 2)
+_max = 0
+for i in range(1, len(n)):
+    if n[i] == 1:
+        tmp = num - 2 ** (len(n) - 1 - i)
+    else:
+        tmp = num + 2 ** (len(n) - 1 - i)
+    _max = max(_max, tmp)
+print(_max)
