f7c5ce1cad7a5d5184320a6313b730cec64bae29
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-07 13:42:32 +0900

    Title: 원 모양으로 되어있는 방, Time: 57ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250607/원 모양으로 되어있는 방/a-room-in-a-circle.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+inone = sum(arr)
+
+_min = 1000000
+for i in range(n):
+    tmp = inone
+    dist = 0
+    for j in range(i, i + n):
+        tmp -= arr[j%n]
+        dist += tmp
+    _min = min(_min, dist)
+print(_min)
