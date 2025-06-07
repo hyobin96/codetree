7a3a6990729558302231c00c70b9f8255de104b2
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-07 18:10:43 +0900

    Title: G or H 2, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250607/G or H 2/G-or-H-2.py
@@ -0,0 +1,31 @@
+n = int(input())
+L, R = 101, -1
+arr = [0] * 101
+for _ in range(n):
+    pos, pic = input().split()
+    pos = int(pos)
+    arr[pos] = pic
+    L = min(L, pos)
+    R = max(R, pos)
+
+def is_same(i, j):
+    g, h = 0, 0
+    for e in arr[i:j+1]:
+        if e == 'G':
+            g += 1
+        elif e == 'H':
+            h += 1
+    if g == h:
+        return True
+    return False
+
+_max = 0
+for i in range(R):
+    if arr[i] != 0:
+        for j in range(i+1, R):
+            if arr[j] != 0:
+                if is_same(i, j):
+                    _max = max(_max, j-i)
+
+print(_max)
+        
