65489d1b48ad458e5589d5e3c77a306f9dbce958
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-07 16:36:47 +0900

    Title: 최고의 13위치 2, Time: 77ms, Memory: 18MB, Status: Passed - Codetree

--- 250607/최고의 13위치 2/best-place-of-13-2.py
+++ 250607/최고의 13위치 2/best-place-of-13-2.py
@@ -6,7 +6,7 @@ for i in range(n):
     for j in range(n):
         for k in range(n - 2):
             if i == j:
-                for l in range(k + 2, n-2):
+                for l in range(k + 3, n-2):
                     _max = max(_max, sum(arr[i][k:k+3]) + sum(arr[j][l:l+3]))
             else:
                 for l in range(n-2):
