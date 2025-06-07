2d042c179711854acd949cadf7ccbdc080d59ccb
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-07 16:35:55 +0900

    Title: 최고의 13위치 2, Time: 89ms, Memory: 18MB, Status: Wrong Answer - Codetree

--- 250607/최고의 13위치 2/best-place-of-13-2.py
+++ 250607/최고의 13위치 2/best-place-of-13-2.py
@@ -3,8 +3,12 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 
 _max = 0
 for i in range(n):
-    for j in range(i + 1, n):
+    for j in range(n):
         for k in range(n - 2):
-            for l in range(n-2):
-                _max = max(_max, sum(arr[i][k:k+3]) + sum(arr[j][l:l+3]))
+            if i == j:
+                for l in range(k + 2, n-2):
+                    _max = max(_max, sum(arr[i][k:k+3]) + sum(arr[j][l:l+3]))
+            else:
+                for l in range(n-2):
+                    _max = max(_max, sum(arr[i][k:k+3]) + sum(arr[j][l:l+3]))
 print(_max)
