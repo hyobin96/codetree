ad3b3ad20c9ec0593ba498213b3cbb66ab460871
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-09 20:12:35 +0900

    Title: 상해버린 치즈, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250609/상해버린 치즈/rotten-cheese.py
+++ 250609/상해버린 치즈/rotten-cheese.py
@@ -5,10 +5,14 @@ s_arr = [tuple(map(int, input().split())) for _ in range(s)]
 m_arr = [0] * (m+1)
 n_arr = [0] * (n+1)
 
-for n1, t1 in s_arr:
-    for n2, m2, t2 in d_arr:
+for n2, m2, t2 in d_arr:
+    is_sang = True
+    for n1, t1 in s_arr:
         if n1 == n2 and t1 > t2:
-            m_arr[m2] = 1
+            is_sang = False
+    if is_sang:
+        m_arr[m2] = 1
+
 for n1, m1, _ in d_arr:
     if m_arr[m1] == 1:
         n_arr[n1] = 1
