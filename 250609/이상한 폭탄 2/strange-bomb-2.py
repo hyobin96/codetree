187eaa937e2f1864e7e45d59ad4f6b48e73711bb
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-09 20:41:21 +0900

    Title: 이상한 폭탄 2, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250609/이상한 폭탄 2/strange-bomb-2.py
+++ 250609/이상한 폭탄 2/strange-bomb-2.py
@@ -3,9 +3,7 @@ bomb = [int(input()) for _ in range(n)]
 
 _max = -1
 for i in range(n):
-    for j in range(0 if i-k < 0 else i-k, n if i+4 > n else i+4):
-        if i == j:
-            continue
+    for j in range(i + 1, n if i+4 > n else i+4):
         if bomb[i] == bomb[j]:
             _max = max(_max, bomb[i])
 
