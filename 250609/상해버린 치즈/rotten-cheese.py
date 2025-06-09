6b2642efe477e44191caf6a5ed8902040c98f6f9
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-09 20:04:34 +0900

    Title: 상해버린 치즈, Time: 62ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250609/상해버린 치즈/rotten-cheese.py
@@ -0,0 +1,16 @@
+n, m, d, s = map(int, input().split())
+d_arr = [tuple(map(int, input().split())) for _ in range(d)]
+s_arr = [tuple(map(int, input().split())) for _ in range(s)]
+
+m_arr = [0] * (m+1)
+n_arr = [0] * (n+1)
+
+for n1, t1 in s_arr:
+    for n2, m2, t2 in d_arr:
+        if n1 == n2 and t1 > t2:
+            m_arr[m2] = 1
+for n1, m1, _ in d_arr:
+    if m_arr[m1] == 1:
+        n_arr[n1] = 1
+
+print(sum(n_arr))
