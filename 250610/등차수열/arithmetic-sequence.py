0788380610a6244d395428a9bf132130383bd045
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-10 23:14:51 +0900

    Title: 등차수열, Time: 49ms, Memory: 17MB, Status: Passed - Codetree

--- 250610/등차수열/arithmetic-sequence.py
+++ 250610/등차수열/arithmetic-sequence.py
@@ -6,13 +6,13 @@ for num in a:
     count_arr[num] = 1
 
 _max = 0
-for k in range(a[-1]):
+for k in range(1, a[-1]):
     cnt = 0
     for num in a:
         if num >= k:
             break
         diff = k - num
-        if count_arr[num+diff] == 1:
+        if count_arr[k+diff] == 1:
             cnt += 1
     _max = max(_max, cnt)
 print(_max)
