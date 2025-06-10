dc1c34fe67581b3b14eaa223d081affc8d3501e2
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-10 17:09:17 +0900

    Title: 스승의 은혜 3, Time: 98ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250610/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 250610/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -9,9 +9,8 @@ for i in range(n):
     for j, (p, s) in enumerate(arr):
         if i == j:
             p //= 2
-        if B < (p+s):
-            break
-        cnt += 1
-        B -= (p+s)
+        if B >= (p+s):
+            cnt += 1
+            B -= (p+s)
     _max = max(_max, cnt)
 print(_max)
