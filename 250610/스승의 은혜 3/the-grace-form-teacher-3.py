add35d876bbcce719dec45378f291a125d4cad8b
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-10 17:14:11 +0900

    Title: 스승의 은혜 3, Time: 109ms, Memory: 18MB, Status: Wrong Answer - Codetree

--- 250610/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 250610/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,16 +1,19 @@
 n, b = map(int, input().split())
-arr = [tuple(map(int, input().split())) for _ in range(n)]
-arr.sort(key=lambda x: (x[0], x[1]))
+arr = [list(map(int, input().split())) for _ in range(n)]
 
 _max = 0
 for i in range(n):
+    arr[i][0] //= 2
+    arr.sort(key=lambda x: x[0]+x[1])
     B = b
     cnt = 0
     for j, (p, s) in enumerate(arr):
         if i == j:
             p //= 2
-        if B >= (p+s):
-            cnt += 1
-            B -= (p+s)
+        if B < (p+s):
+            break
+        cnt += 1
+        B -= (p+s)
     _max = max(_max, cnt)
+    arr[i][0] *= 2
 print(_max)
