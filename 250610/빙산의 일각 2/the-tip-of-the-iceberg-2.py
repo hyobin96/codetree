216a0b2a79e621726e2e46ba91799cf52dd775c2
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-10 23:37:35 +0900

    Title: 빙산의 일각 2, Time: 58ms, Memory: 17MB, Status: Passed - Codetree

--- 250610/빙산의 일각 2/the-tip-of-the-iceberg-2.py
+++ 250610/빙산의 일각 2/the-tip-of-the-iceberg-2.py
@@ -3,13 +3,16 @@ H_list = [int(input()) for _ in range(N)]
 
 _max = 0
 for x in range(1000):
-    cnt = 0
+    cnt1 = 0
+    cnt2 = 0
     for i in range(N):
-        if i == N-1:
-            if H_list[i] > x and H_list[i-1] <= x:
-                cnt += 1
-        else:
-            if H_list[i] > x and H_list[i+1] <= x:
-                cnt += 1
-    _max = max(_max, cnt)
+        if cnt2 != 0:
+            if H_list[i] <= x:
+                cnt1 += 1
+                cnt2 = 0
+        elif H_list[i] > x:
+            cnt2 += 1
+    if cnt2 != 0:
+        cnt1 += 1
+    _max = max(_max, cnt1)
 print(_max)
