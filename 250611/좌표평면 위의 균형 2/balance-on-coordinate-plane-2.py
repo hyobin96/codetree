d3cf017f07f274afe9095707715548d65fec67a9
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-11 12:23:42 +0900

    Title: 좌표평면 위의 균형 2, Time: 77ms, Memory: 18MB, Status: Passed - Codetree

--- 250611/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.py
+++ 250611/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.py
@@ -4,7 +4,7 @@ xys = [tuple(map(int, input().split())) for _ in range(N)]
 _min = 2000000000
 for x_p in range(0, 101, 2):
     for y_p in range(0, 101, 2):
-        cnts = [0, 0, 0, 0]
+        cnts = [0] * 4
         for x, y in xys:
             if  x > x_p and y > y_p:
                 cnts[0] += 1
