a859d7fe1c2a3a18f28c62753c9b2a9e3b50477f
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-11 11:11:49 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250611/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.py
+++ 250611/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.py
@@ -2,8 +2,8 @@ A, B, C = map(int, input().split())
 
 _max = 0
 for i in range(C // A + 1):
-    for j in range(C // B + 1):
-        _sum = A * i + B * j
-        if _sum <= C:
-            _max = max(_max, _sum)
+    a = A * i
+    num_b = (C - a) // B
+    _sum = a + B * num_b
+    _max = max(_max, _sum)
 print(_max)
