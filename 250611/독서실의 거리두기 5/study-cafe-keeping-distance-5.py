970f5ed3f4cb4c876bd70d7e8ca89e5f76283298
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-11 15:57:54 +0900

    Title: 독서실의 거리두기 5, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250611/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 250611/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -1,18 +1,30 @@
 N = int(input())
 arr = list(map(int, list(input())))
 _max = 0
-for i in range(N):
-    if arr[i] == 1:
-        continue
-    l, r = i, i
-    _min = 2000000000
+
+def get_dist(j):
+    l, r = j, j
     dist = 0
     while l != 0 or r != N-1:
         l = l-1 if l-1 >= 0 else 0
         r = r+1 if r+1 <= N-1 else N-1 
         dist += 1
-        if arr[l] == 1 or arr[r] == 1:
-            _min = min(_min, dist)
+        if l != j and arr[l] == 1:
             break
+        if r != j and arr[r] == 1:
+            break
+    return dist
+
+for i in range(N):
+    if arr[i] == 1:
+        continue
+    arr[i] = 1
+    _min = 2000000000
+    for j in range(N):
+        if arr[j] == 0:
+            continue
+        dist = get_dist(j)
+        _min = min(_min, dist)
     _max = max(_max, _min)
+    arr[i] = 0
 print(_max)
