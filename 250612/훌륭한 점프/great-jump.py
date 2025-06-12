388ecd97e0ebffae44cf5a55eaa0a8c05fa1ff58
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-12 12:11:18 +0900

    Title: 훌륭한 점프, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250612/훌륭한 점프/great-jump.py
@@ -0,0 +1,24 @@
+N, K = map(int, input().split())
+arr = list(map(int, input().split()))
+
+def is_possible(max_value):
+    pos_elems = []
+    for i, v in enumerate(arr):
+        if v <= max_value:
+            pos_elems.append(i)
+    
+    for i in range(len(pos_elems) - 1):
+        dist = pos_elems[i+1] - pos_elems[i]
+        if dist > K:
+            return False
+    
+    return True
+
+min_value = 0
+for max_value in range(max(arr), 0, -1):
+    if is_possible(max_value):
+        min_value = max_value
+    else:
+        break
+
+print(min_value)
