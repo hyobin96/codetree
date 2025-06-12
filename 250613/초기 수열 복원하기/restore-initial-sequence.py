9d7cf67f871dda3831c042a2f51de50465f4891d
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-13 00:29:11 +0900

    Title: 초기 수열 복원하기, Time: 59ms, Memory: 17MB, Status: Passed - Codetree

--- 250613/초기 수열 복원하기/restore-initial-sequence.py
+++ 250613/초기 수열 복원하기/restore-initial-sequence.py
@@ -3,24 +3,21 @@ arr = list(map(int, input().split()))
 
 for i in range(1, N+1):
     curr = i
-    nxt = 1
     pointer = 0
     count_arr = [0] * (N+1)
     count_arr[curr] = 1
     ans_arr = [curr]
 
     while pointer < N-1:
-        if arr[pointer] == curr + nxt:
-            if curr == nxt or count_arr[nxt] == 1:
-                break
+        nxt = abs(arr[pointer] - curr)
+        if nxt > N or nxt == curr or count_arr[nxt] == 1:
+            break
 
-            ans_arr.append(nxt)
-            count_arr[nxt] = 1
-            curr, nxt = nxt, 1
-            pointer += 1
-        
-        nxt += 1
-
+        ans_arr.append(nxt)
+        count_arr[nxt] = 1
+        curr = nxt
+        pointer += 1
+    
 
     if len(ans_arr) == N:
         print(*ans_arr)
