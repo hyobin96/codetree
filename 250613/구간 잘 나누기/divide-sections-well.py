811472d38388ed2daaed880dfebe7028129d5095
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-13 15:04:13 +0900

    Title: 구간 잘 나누기, Time: 64ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250613/구간 잘 나누기/divide-sections-well.py
@@ -0,0 +1,26 @@
+N, M = map(int, input().split())
+num_arr = list(map(int, input().split()))
+
+MAX_NUM = sum(num_arr)
+
+def is_possible(max_num):
+    remain_m_cnt = M - 1
+
+    num_sum = 0
+    for num in num_arr:
+        num_sum += num
+        if num_sum > max_num:
+            if remain_m_cnt == 0:
+                return False
+
+            remain_m_cnt -= 1
+            num_sum = num
+    
+    return True
+
+ans = MAX_NUM
+for max_num in range(MAX_NUM, 0, -1):
+    if is_possible(max_num):
+        ans = max_num
+
+print(ans)
