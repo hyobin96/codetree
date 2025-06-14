31522f6256194a66a5bdfeef92da5e0541d46905
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-14 23:18:47 +0900

    Title: 코딩톡, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250614/코딩톡/coding-talk.py
@@ -0,0 +1,18 @@
+N, M, p = map(int, input().split())
+m_arr = []
+person_arr = [1] * N
+
+for _ in range(M):
+    c, u = input().split()
+    c = ord(c) - ord('A') 
+    u = int(u)
+    m_arr.append((c, u))
+
+for i in range(p - 1, M):
+    c, u = m_arr[i]
+    person_arr[c] = 0
+
+for i, v in enumerate(person_arr):
+    if v == 1:
+        print(chr(i + ord('A')), end = ' ')
+
