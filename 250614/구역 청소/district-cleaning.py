9b782036b0dad3e599c312334852cb2563ad9d3e
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-14 12:16:01 +0900

    Title: 구역 청소, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250614/구역 청소/district-cleaning.py
+++ 250614/구역 청소/district-cleaning.py
@@ -4,13 +4,13 @@ c, d = map(int, input().split())
 if b < c or a > d:
     area = b - a + c - d
 else:
-    if a < c and d < b:
+    if a <= c and d <= b:
         area = b - a
-    elif c < a and b < d:
+    elif c <= a and b <= d:
         area = d - c
     elif a < c:
         area = d - a
     else:
         area = b - c
-
+                    
 print(area)
