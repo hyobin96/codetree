e2e99a8727fabfc6eff1377366b51caeb31e2514
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-14 18:40:20 +0900

    Title: 전부 겹치는 선분, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 250614/전부 겹치는 선분/overlapping-line-segments.py
+++ 250614/전부 겹치는 선분/overlapping-line-segments.py
@@ -1,25 +1,14 @@
 N = int(input())
 arr = [tuple(map(int, input().split())) for _ in range(N)]
-MIN_NUM, MAX_NUM = 1, 100
 
+x1_max = 0
+x2_min = 100
+for x1, x2 in arr:
+    x1_max = max(x1_max, x1)
+    x2_min = min(x2_min, x2)
 
-def is_overlap(x):
-    cnt = 0
-    
-    for x1, x2 in arr:
-        if x1 <= x <= x2:
-            cnt += 1
-    
-    if cnt == N:
-        return True
-
-    return False
-
-ans = 'No'
-
-for x in range(MIN_NUM, MAX_NUM + 1):
-    if is_overlap(x):
-        ans = 'Yes'
-        break
+ans = 'Yes'
+if x1_max > x2_min:
+    ans = 'No'
 
 print(ans)        
