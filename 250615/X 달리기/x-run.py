0136fb51771247a8aa552dcba1e23020cf0da139
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-15 11:32:30 +0900

    Title: X 달리기, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 250615/X 달리기/x-run.py
+++ 250615/X 달리기/x-run.py
@@ -1,18 +1,16 @@
+import math
+
 X = int(input())
 
-s = 0
-v = 0
-t = 0
-for n in range(1, X + 1):
-    s += n
+v = int(math.sqrt(X))
+s = v * (v + 1) // 2
+t = v
 
-    if s >= X // 2:
-        v = n - 1
-        t = n
-        break
 while s < X:
+    if X - s < v * (v + 1) // 2:
+        v -= 1
+
     s += v
-    v = v - 1 if v - 1 > 0 else 1
     t += 1
 
 print(t)
