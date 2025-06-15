e99ab801b5a1d4217bcf0f9a77456004d901a71e
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-15 23:36:39 +0900

    Title: 연속된 숫자 만들기 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250615/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -0,0 +1,9 @@
+arr = list(map(int, input().split()))
+arr2 = sorted(arr)
+
+if arr2[1] - arr2[0] == 1 and arr[2] - arr2[1] == 1:
+    print(0)
+elif abs(arr[0] - arr[1]) == 2 or abs(arr[1] - arr[2]) == 2 or abs(arr[2] - arr[0]) == 2:
+    print(1)
+else:
+    print(2)
