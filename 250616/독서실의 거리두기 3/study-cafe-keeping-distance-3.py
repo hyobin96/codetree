149e3e38cc20f497125adbe9d40c9ccb9c84d438
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-16 10:27:06 +0900

    Title: 독서실의 거리두기 3, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250616/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -0,0 +1,29 @@
+N = int(input())
+arr = list(map(int, list(input())))
+
+def get_max_dist_pos_x1_x2():
+    x1, x2 = 0, 0
+    max_dist = 0
+
+    for i in range(N):
+        for j in range(i + 1, N):
+            if arr[i] == 1 and arr[j] == 1:
+                if j - i > max_dist:
+                    x1, x2 = i, j
+
+    return x1, x2
+
+def get_min_dist():
+    min_dist = 1000
+    for i in range(N):
+        for j in range(i + 1, N):
+            if arr[i] == 1 and arr[j] == 1:
+                min_dist = min(min_dist, j - i)
+    
+    return min_dist
+
+x1, x2 = get_max_dist_pos_x1_x2()
+arr[(x1 + x2) // 2] = 1
+
+dist = get_min_dist()
+print(dist)
