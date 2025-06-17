bce698327f721a5d33488a8291596bf012f811d5
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-17 11:23:48 +0900

    Title: L, R 그리고 B, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

--- 250617/L, R 그리고 B/l-r-and-b.py
+++ 250617/L, R 그리고 B/l-r-and-b.py
@@ -15,7 +15,8 @@ def get_dist():
     return abs(L[0] - B[0]) + abs(L[1] - B[1]) - 1
 
 dist = 0
-if L[0] == B[0] == R[0] or L[1] == B[1] == R[1]:
+if (L[0] == B[0] == R[0] and (L[1] < R[1] < B[1] or B[1] < R[1] < L[1])) \
+    or (L[1] == B[1] == R[1] and (L[0] < R[0] < B[0] or B[0] < R[0] < L[0])):
     dist = get_dist() + 2
 
 else:
