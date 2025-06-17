cf7dc7a12e15ad82dfd5043984b2e2082fae43f4
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-17 21:44:51 +0900

    Title: 홀수 짝수의 묶음, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250617/홀수 짝수의 묶음/odd-even-bundle.py
+++ 250617/홀수 짝수의 묶음/odd-even-bundle.py
@@ -12,12 +12,17 @@ for n in arr:
 
 cnt = min(odd_cnt, even_cnt)    
 
-if odd_cnt % 3 == 0:
-    cnt += odd_cnt // 3 * 2
-else:
-    if odd_cnt % 3 == 1:
-        cnt += odd_cnt // 3 * 2 - 1
+if odd_cnt > even_cnt:
+
+    if odd_cnt % 3 == 0:
+        cnt += odd_cnt // 3 * 2
     else:
-        cnt += odd_cnt // 3 * 2 + 1
+        if odd_cnt % 3 == 1:
+            cnt += odd_cnt // 3 * 2 - 1
+        else:
+            cnt += odd_cnt // 3 * 2 + 1
+
+elif even_cnt > odd_cnt:
+    cnt += 1
 
 print(cnt)
