1a96edf04e21fd35d8af831c85931ac63848d45b
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-17 20:47:47 +0900

    Title: 최소 와이파이 수, Time: 78ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 250617/최소 와이파이 수/minimum-number-of-wifi.py
@@ -0,0 +1,19 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+curr = 0
+cnt = 0
+
+while curr < n:
+    if arr[curr] == 1:
+        for i in range(curr + 1, curr + m + 1):
+            if arr[i] == 1:
+                curr = i
+
+        cnt += 1
+        curr += m + 1
+
+    else:
+        curr += 1
+
+print(cnt)
