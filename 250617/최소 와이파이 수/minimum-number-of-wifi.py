819849dcbe0065b41abd929e40bb108020dc170e
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-17 20:48:36 +0900

    Title: 최소 와이파이 수, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250617/최소 와이파이 수/minimum-number-of-wifi.py
+++ 250617/최소 와이파이 수/minimum-number-of-wifi.py
@@ -6,7 +6,7 @@ cnt = 0
 
 while curr < n:
     if arr[curr] == 1:
-        for i in range(curr + 1, curr + m + 1):
+        for i in range(curr + 1, curr + m + 1 if curr + m + 1 < n else n):
             if arr[i] == 1:
                 curr = i
 
