592f15cc1271873c5bf57a83de39fc6f48894b6d
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-17 11:58:18 +0900

    Title: 순위 경쟁, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250617/순위 경쟁/ranking-competition.py
@@ -0,0 +1,53 @@
+N = int(input())
+arr = []
+for _ in range(N):
+    c, s = input().split()
+    s = int(s)
+    arr.append((c, s))
+
+def bunru():
+    if A == B == C:
+        return 0
+    
+    if A == B > C:
+        return 1
+    
+    if A == C > B:
+        return 2
+
+    if A > B > C:
+        return 3
+
+    if A > C > B:
+        return 4
+    
+    if B > A > C:
+        return 5
+    
+    if B > C > A:
+        return 6
+
+    if C > A > B:
+        return 7
+
+    if C > B > A:
+        return 8
+
+value_arr = [0] * 3
+prev = 0
+cnt = 0
+A, B, C = 0, 0, 0
+for c, s in arr:
+    if c == 'A':
+        A += s
+    elif c == 'B':
+        B += s
+    else:
+        C += s
+
+    curr = bunru()
+    if prev != curr:
+        cnt += 1
+        prev = curr
+
+print(cnt)
