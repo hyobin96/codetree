70a4b5b372dc8d93fcfb2c58786f6b841c118251
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-18 12:33:46 +0900

    Title: 수열의 순서 바꾸기, Time: 55ms, Memory: 17MB, Status: Passed - Codetree

--- 250618/수열의 순서 바꾸기/reorder-sequence.py
+++ 250618/수열의 순서 바꾸기/reorder-sequence.py
@@ -9,21 +9,20 @@ for _ in range(N):
         arr.pop(0)
         cnt += 1
     else:
-        is_ok = False
+        idx = -1
         for i in range(N-1, 0, -1):
             if arr[i] > arr[0] and arr[i-1] < arr[0]:
-                arr.insert(i, arr[0])
-                arr.pop(0)
-                is_ok = True
-                cnt += 1
+                idx = i
                 break
 
-        if not is_ok:
-            for i in range(N-1, 0, -1):
-                if arr[i] > arr[0] and arr[i-1] > arr[0] and arr[i] < arr[i-1]:
-                    arr.insert(i, arr[0])
-                    arr.pop(0)
-                    cnt += 1
-                    break
+        for i in range(N-1, 0, -1):
+            if arr[i] > arr[0] and arr[i-1] > arr[0] and arr[i] < arr[i-1]:
+                idx = max(idx, i)
+                break
+
+        if idx != -1:
+            arr.insert(idx, arr[0])
+            arr.pop(0)
+            cnt += 1
             
 print(cnt)
