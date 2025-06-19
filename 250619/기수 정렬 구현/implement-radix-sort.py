e56a1321ead8eab6fb7f5d3fc4eba200da8549fb
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-19 18:51:14 +0900

    Title: 기수 정렬 구현, Time: 84ms, Memory: 20MB, Status: Runtime Error - Codetree

---
+++ 250619/기수 정렬 구현/implement-radix-sort.py
@@ -0,0 +1,20 @@
+n = int(input())
+nums = list(map(int, input().split()))
+
+mod = 10
+for _ in range(len(str(max(nums)))):
+    arr = [[] for _ in range(10)]
+    for num in nums:
+        remains = num % mod - num % (mod // 10)
+        arr[remains].append(num)
+
+    mod *= 10
+
+    store_arr = []
+    for i in range(10):
+        for j in range(len(arr[i])):
+            store_arr.append(arr[i][j])
+
+    nums = store_arr
+
+print(*nums)
