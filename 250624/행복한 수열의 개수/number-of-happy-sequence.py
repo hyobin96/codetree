2fe6d90dd99f7ccd397660db902b83c759da91ec
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-24 14:00:14 +0900

    Title: 행복한 수열의 개수, Time: 69ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250624/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,34 @@
+N, M = map(int, input().split())
+
+arr = [list(map(int, input().split())) for _ in range(3)]
+
+def is_manzok(i, j):
+    for k in range(j, j + M):
+        if arr[i][k] != arr[i][j]:
+            return False
+
+    return True
+
+def is_manzok2(j, i):
+    for k in range(i, i + M):
+        if arr[j][i] != arr[j][k]:
+            return False
+
+    return True
+
+cnt = 0
+for i in range(N):
+    for j in range(N - M + 1):
+        if is_manzok(i, j):
+            cnt += 1
+            break
+
+for j in range(N):
+    for i in range(N - M + 1):
+        if is_manzok2(j, i):
+            cnt += 1
+            break
+
+print(cnt)
+
+    
