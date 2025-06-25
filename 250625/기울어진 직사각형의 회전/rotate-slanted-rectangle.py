74338bffafb70c066afb1d1714b2ea7cb8ed489e
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-25 14:43:17 +0900

    Title: 기울어진 직사각형의 회전, Time: 62ms, Memory: 17MB, Status: Passed - Codetree

--- 250625/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
+++ 250625/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
@@ -2,106 +2,34 @@ N = int(input())
 
 arr = [list(map(int, input().split())) for _ in range(N)]
 
+
+def wise(r, c, m1, m2, m3, m4, dire):
+    if dire == 0:
+        drs, dcs = [-1, -1, 1, 1], [1, -1, -1, 1]
+        dist = [m1, m2, m3, m4]
+
+    else:
+        drs, dcs = [-1, -1, 1, 1], [-1, 1, 1, -1]
+        dist = [m2, m3, m4, m1]
+
+    temp = [[] for _ in range(N)]
+
+    for i in range(N):
+        temp[i] = arr[i][::]
+
+    for dr, dc, d in zip(drs, dcs, dist):
+        for _ in range(d):
+            nr = r + dr
+            nc = c + dc
+
+            arr[nr][nc] = temp[r][c]
+
+            r, c = nr, nc
+
 r, c, m1, m2, m3, m4, dire = map(int, input().split())
 r, c = r - 1, c - 1
 
-
-dr, dc = [-1, -1, 1, 1], [1, -1, -1, 1]
-
-dis = [m1, m2, m3, m4]
-
-rcs = [[] for _ in range(4)]
-
-for d in range(4):
-    rcs[d].append((r, c))
-    for _ in range(dis[d]):
-        r += dr[d]
-        c += dc[d]
-
-        rcs[d].append((r, c))
-
-# print(rcs)
-
-def counterclockwise():
-    temp1 = arr[rcs[0][-1][0]][rcs[0][-1][1]]
-
-    for i in range(m1, 0, -1):
-        r1, c1 = rcs[0][i]
-        r2, c2 = rcs[0][i - 1]
-
-        arr[r1][c1] = arr[r2][c2]
-
-    # for i in range(N):
-    #     print(*arr[i])
-
-    temp2 = arr[rcs[3][0][0]][rcs[3][0][1]]
-
-    for i in range(m3, 0, -1):
-        r1, c1 = rcs[2][i]
-        r2, c2 = rcs[2][i - 1]
-
-        arr[r1][c1] = arr[r2][c2]
-
-
-    for i in range(m2, 0, -1):
-        r1, c1 = rcs[1][i]
-        r2, c2 = rcs[1][i - 1]
-
-        arr[r1][c1] = arr[r2][c2]
-
-    for i in range(m4, 0, -1):
-        r1, c1 = rcs[3][i]
-        r2, c2 = rcs[3][i - 1]
-
-        arr[r1][c1] = arr[r2][c2]
-
-    r1, c1 = rcs[1][1]
-    arr[r1][c1] = temp1
-    
-    r1, c1 = rcs[3][1]
-    arr[r1][c1] = temp2
-
-
-def clockwise():
-    temp1 = arr[rcs[0][0][0]][rcs[0][0][1]]
-    for i in range(m1):
-        r1, c1 = rcs[0][i]
-        r2, c2 = rcs[0][i + 1]
-
-        arr[r1][c1] = arr[r2][c2]
-
-    temp2 = arr[rcs[2][0][0]][rcs[2][0][1]]
-
-    for i in range(m3):
-        r1, c1 = rcs[2][i]
-        r2, c2 = rcs[2][i + 1]
-
-        arr[r1][c1] = arr[r2][c2]
-
-    for i in range(m2):
-        r1, c1 = rcs[1][i]
-        r2, c2 = rcs[1][i + 1]
-
-        arr[r1][c1] = arr[r2][c2]
-
-    for i in range(m4):
-        r1, c1 = rcs[3][i]
-        r2, c2 = rcs[3][i + 1]
-
-        arr[r1][c1] = arr[r2][c2]
-
-    r1, c1 = rcs[3][-2]
-    arr[r1][c1] = temp1
-    
-    r1, c1 = rcs[1][-2]
-    arr[r1][c1] = temp2    
-
-
-if dire == 0:
-    counterclockwise()
-
-else:
-    clockwise()
+wise(r, c, m1, m2, m3, m4, dire)
 
 for i in range(N):
     print(*arr[i])
