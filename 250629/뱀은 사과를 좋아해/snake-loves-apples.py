300c5f1dbbdf1f5e9651d0e9e4e5d828b746009f
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-29 16:49:32 +0900

    Title: 뱀은 사과를 좋아해, Time: 51ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250629/뱀은 사과를 좋아해/snake-loves-apples.py
@@ -0,0 +1,59 @@
+import sys
+
+def in_range(r, c):
+    return 0 <= r < N and 0 <= c < N
+
+
+mapper = {'U':0, 'D':1, 'L':2, 'R':3}
+drs, dcs = [-1, 1, 0, 0], [0, 0, -1, 1]
+
+tail = [0, 0]
+head = [0, 0]
+
+N, M, K = map(int, input().split())
+
+arr = [[0] * N for _ in range(N)]
+
+arr[0][0] = 1
+
+for _ in range(M):
+    x, y = map(int, input().split())
+    arr[x - 1][y - 1] = 2
+
+query = [input().split() for _ in range(K)]
+
+t = 0
+r, c = head
+for d, p in query:
+    p = int(p)
+    d = mapper[d]
+
+    for _ in range(p):
+        nr, nc = r + drs[d], c + dcs[d]
+
+        if not in_range(nr, nc):
+            t += 1
+            print(t)
+            sys.exit()
+
+        if arr[nr][nc] == 2:
+            arr[nr][nc] = 1
+            r, c = nr, nc
+            
+        elif arr[nr][nc] == 1:
+            t += 1
+            print(t)
+            sys.exit()
+
+        else:
+            tr, tc = tail
+            arr[tr][tc] = 0
+            arr[nr][nc] = 1
+            r, c = nr, nc
+        
+        t += 1
+
+print(t)
+
+
+
