38bbbc9996314a418721739d9f3c6a889791439f
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-06-29 11:53:21 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 1082ms, Memory: 17MB, Status: Time Limit Exceed - Codetree

---
+++ 250629/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -0,0 +1,50 @@
+def in_range(r, c):
+    return 0 <= r < N and 0 <= c < N
+
+def is_right_exist(r, c, d):
+    d = (d + 1) % 4
+    return arr[r + drs[d]][c + dcs[d]] == '#'
+
+def is_exist(r, c, d):
+    return arr[r + drs[d]][c + dcs[d]] == '#'
+
+
+drs, dcs = [0, 1, 0, -1], [1, 0, -1, 0]
+
+N = int(input())
+x, y = map(int, input().split())
+r, c = x - 1, y - 1
+
+arr = [list(input()) for _ in range(N)]
+
+d = 0
+T = 0
+while in_range(r, c):
+    if T > N ** 2:
+        break
+
+    if is_right_exist(r, c, d):
+        if not in_range(r + drs[d], c + dcs[d]):
+            r, c = r + drs[d], c + dcs[d]
+            T += 1
+            break
+
+        elif is_exist(r, c, d):
+            d = (d + 3) % 4
+
+        else:
+            r, c = r + drs[d], c + dcs[d]
+            T += 1
+
+    else:
+        d = (d + 1) % 4
+        r, c = r + drs[d], c + dcs[d]
+        T += 1
+
+    
+if T > N ** 2:
+    print(-1)
+else:
+    print(T)
+
+
