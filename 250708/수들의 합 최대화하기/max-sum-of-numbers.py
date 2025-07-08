b1a650ac0673fde30433ed4cb114341fe4b187e4
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-07-08 20:20:53 +0900

    Title: 수들의 합 최대화하기, Time: 171ms, Memory: 22MB, Status: Passed - Codetree

--- 250708/수들의 합 최대화하기/max-sum-of-numbers.py
+++ 250708/수들의 합 최대화하기/max-sum-of-numbers.py
@@ -1,22 +1,22 @@
-def select_nums(cnt, result):
-    global row_visited, col_visited, answer, grid
+def select_nums(cnt, result, start):
+    global col_visited, answer, grid
     if cnt == N:
         answer = max(answer, result)
         return
 
-    for i in range(N):
+    for i in range(start, N):
         for j in range(N):
-            if row_visited[i] or col_visited[j]:
+            if col_visited[j]:
                 continue
 
-            row_visited[i], col_visited[j] = True, True
-            select_nums(cnt + 1, result + grid[i][j])
-            row_visited[i], col_visited[j] = False, False
+            col_visited[j] = True
+            select_nums(cnt + 1, result + grid[i][j], i + 1)
+            col_visited[j] = False
 
     
 N = int(input())
 grid = [list(map(int, input().split())) for _ in range(N)]
-row_visited, col_visited = [False] * N, [False] * N
+col_visited = [False] * N
 answer = 0
-select_nums(0, 0)
+select_nums(0, 0, 0)
 print(answer)
