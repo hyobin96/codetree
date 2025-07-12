5a1c7d5c4446217551d55ca862480847f122b754
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-07-12 22:12:04 +0900

    Title: 비를 피하기, Time: 105ms, Memory: 19MB, Status: Passed - Codetree

--- 250712/비를 피하기/stay-out-of-rain.py
+++ 250712/비를 피하기/stay-out-of-rain.py
@@ -1,28 +1,30 @@
 from collections import deque
 
+q = deque()
+
 in_range = lambda r, c: 0 <= r < N and 0 <= c < N
 can_go = lambda r, c: in_range(r, c) and grid[r][c] != 1 and visited[r][c] != cir
 
-def bfs(curr):
-    global visited
-    q = deque()
+def push(curr):
+    global visited, q
+    r, c, _ = curr
+    visited[r][c] = cir
     q.append(curr)
+
+def bfs():
+    global visited, q
 
     drs, dcs = (-1, 1, 0, 0), (0, 0, -1, 1)
     
     while q:
-        r, c, dist = q.popleft()
-        if grid[r][c] == 3:
-            return dist
-            break
+        r, c, cnt = q.popleft()
+        if grid[r][c] == 2:
+            dist[r][c] = cnt
 
         for dr, dc in zip(drs, dcs):
             nr, nc = r + dr, c + dc
             if can_go(nr, nc):
-                visited[nr][nc] = cir
-                q.append((nr, nc, dist + 1))
-
-    return -1
+                push((nr, nc, cnt + 1))
 
 N, H, M = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(N)]
@@ -32,10 +34,12 @@ cir = 1
 
 for i in range(N):
     for j in range(N):
-        if grid[i][j] == 2:
-            visited[i][j] = cir
-            dist[i][j] = bfs((i, j, 0))
-            cir += 1
-        
+        if grid[i][j] == 3:
+            push((i, j, 0))
+        elif grid[i][j] == 2:
+            dist[i][j] = -1
+
+bfs()        
+
 for i in range(N):
     print(*dist[i])
