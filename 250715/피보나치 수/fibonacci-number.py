e2d997a3e8b328cfd0a01482eb1e4ae5d3c72355
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-07-15 15:14:56 +0900

    Title: 피보나치 수, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250715/피보나치 수/fibonacci-number.py
+++ 250715/피보나치 수/fibonacci-number.py
@@ -1,7 +1,7 @@
 N = int(input())
 
 def fibo():
-    fibo = [-1] * (N + 1)
+    fibo = [-1] * (N + 2)
     fibo[1], fibo[2] = 1, 1
     for i in range(3, N + 1):
         fibo[i] = fibo[i - 1] + fibo[i - 2]
