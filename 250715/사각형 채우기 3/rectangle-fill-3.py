236a8be47b364c3feebb89ddb6d8c8415a7a00ea
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-07-15 22:50:04 +0900

    Title: 사각형 채우기 3, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250715/사각형 채우기 3/rectangle-fill-3.py
+++ 250715/사각형 채우기 3/rectangle-fill-3.py
@@ -1,7 +1,8 @@
 N = int(input())
 dp = [0] * (N + 2)
-dp[1], dp[2] = 2, 7
+dp[0], dp[1], dp[2] = 1, 2, 7
 for i in range(3, N + 1):
-    dp[i] = (2 * dp[i - 1] + 4 * dp[i - 2]) % 1000000007
+    dp[i] = (2 * dp[i - 1] + 3 * dp[i - 2]) % 1000000007
+    dp[i] = (dp[i] + (sum(dp[:i - 2]) * 2)) % 1000000007
 
 print(dp[N])
