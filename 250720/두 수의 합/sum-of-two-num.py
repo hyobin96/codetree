6dce550ced083dc24c155504cf1567467c2fcf3e
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-07-20 15:14:34 +0900

    Title: 두 수의 합, Time: 76ms, Memory: 26MB, Status: Passed - Codetree

--- 250720/두 수의 합/sum-of-two-num.py
+++ 250720/두 수의 합/sum-of-two-num.py
@@ -7,20 +7,13 @@ for z in numbers:
     else:
         z_d[z] = 1
 
-
-def fact(n):
-    result = 1
-    for i in range(1, n + 1):
-        result *= i
-    return result
-
 answer1 = 0
 answer2 = 0
 for z in z_d.keys():
     target = K - z
     if target in z_d:
         if target == z and z_d[target] > 1:
-            answer2 += fact(z_d[target] - 1)
+            answer2 += sum(range(z_d[target]))
         else:
             answer1 += z_d[target] * z_d[z]
 
