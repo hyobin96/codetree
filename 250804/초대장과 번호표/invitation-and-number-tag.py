27476908b1ab9aabf5b49cf48c44bea4df43de4d
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-08-04 21:56:42 +0900

    Title: 초대장과 번호표, Time: 1096ms, Memory: 25MB, Status: Time Limit Exceed - Codetree

--- 250804/초대장과 번호표/invitation-and-number-tag.py
+++ 250804/초대장과 번호표/invitation-and-number-tag.py
@@ -10,24 +10,20 @@ for _ in range(G):
 
 # Please write your code here.
 
-# d = dict()
-
-# for i in range(G):
-#     if group_size[i] in d:
-#         d[group_size[i]].append(group[i])
-#     else:
-#         d[group_size[i]] = [group[i]]
-
-for g in group:
-    g.sort()
-group.sort(lambda x: (len(x), x))
+# for g in group:
+#     g.sort()
+# group.sort(lambda x: (len(x), x))
 
 answers = {1}
-for _ in range(int(G ** 0.5) + 1):
+while True:
+    size = len(answers)
     for g in group:
         s = set(g)
         sub = s - answers
         if len(sub) == 1:
             answers |= sub
+
+    if size == len(answers):
+        break
 
 print(len(answers))
