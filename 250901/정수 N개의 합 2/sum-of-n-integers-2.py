d51e5fcdb0f07c7c516e5639f60cbae84d32d200
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-09-01 17:45:25 +0900

    Title: 정수 N개의 합 2, Time: 60ms, Memory: 22MB, Status: Wrong Answer - Codetree

---
+++ 250901/정수 N개의 합 2/sum-of-n-integers-2.py
@@ -0,0 +1,16 @@
+n, k = map(int, input().split())
+
+arr = list(map(int, input().split()))
+
+s = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    s[i] = s[i - 1] + arr[i - 1]
+
+
+answer = -100
+
+for i in range(1, n + 1):
+    answer = max(s[i] - s[i - k], answer)
+
+print(answer)
