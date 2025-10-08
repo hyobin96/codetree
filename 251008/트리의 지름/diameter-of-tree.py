415bd495c654a2cb04d529b9dd5d963babc23fcd
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-10-08 18:25:16 +0900

    Title: 트리의 지름, Time: 353ms, Memory: 39MB, Status: Runtime Error - Codetree

---
+++ 251008/트리의 지름/diameter-of-tree.py
@@ -0,0 +1,41 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(n - 1):
+    u, v, w = map(int, input().split())
+
+    graph[u].append((v, w))
+    graph[v].append((u, w))
+
+
+target = 0
+weight = 0
+
+def dfs(u, w, visited):
+    global target, weight
+
+    visited[u] = 1
+    if weight < w:
+        target = u
+        weight = w
+
+    for v, wei in graph[u]:
+        if visited[v]:
+            continue
+
+        dfs(v, w + wei, visited)
+
+
+dfs(1, 0, [0] * (n + 1))
+
+weight = 0
+
+dfs(target, 0, [0] * (n + 1))
+
+print(weight)
+
