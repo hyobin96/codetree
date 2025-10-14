fc1297eaeb0245e2af601e6a54ee50193055949c
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2025-10-14 19:15:01 +0900

    Title: 최대 직사각형 합, Time: 341ms, Memory: 21MB, Status: Wrong Answer - Codetree

--- 251014/최대 직사각형 합/max-rect-sum-in-grid.py
+++ 251014/최대 직사각형 합/max-rect-sum-in-grid.py
@@ -14,18 +14,24 @@ for i in range(1, n + 1):
         prefix_grid[i][j] = prefix_grid[i - 1][j] + prefix_grid[i][j - 1] - prefix_grid[i - 1][j - 1] + grid[i - 1][j - 1]
 
 
+# r1, c1 은 왼쪽 위 꼭짓점, r2, c2는 오른쪽 아래 꼭짓점
+def get_area(r1, c1, r2, c2):
+    return prefix_grid[r2][c2] -  prefix_grid[r1 - 1][c2] - prefix_grid[r1][c1 - 1] + prefix_grid[r1 - 1][c1 - 1]
+
 answer = -1000000000
 
- # 0 0 0 0
-# 세로 길이
-for i in range(1, n + 1):
-    # 가로 길이
-    for j in range(1, n + 1):
+# 시작 행
+for i1 in range(1, n + 1):
+    # 끝 행
+    for i2 in range(i1, n + 1):
+        _sums = [0] * (n + 1)
+        prefix_sum = [0] * (n + 1)
 
-        for k in range(i, n + 1):
-            for l in range(j, n + 1):  # k - i, l - j
-                _sum = prefix_grid[k][l] - prefix_grid[k - i][l] - prefix_grid[k][l - j] + prefix_grid[k - i][l - j]
-                answer = max(answer, _sum)
+        for j in range(1, n + 1):
+            _sums[j] = get_area(i1, j, i2, j)
+            prefix_sum[j] = prefix_sum[j - 1] + _sums[j]
+            answer = max(answer, prefix_sum[j])
+        
 
 print(answer)
         
