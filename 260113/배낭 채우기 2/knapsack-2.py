04f37c43897f3bd56a24482c9fe0a5346d36fc95
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2026-01-13 23:45:03 +0900

    Title: 배낭 채우기 2, Time: 64ms, Memory: 17MB, Status: Passed - Codetree

--- 260113/배낭 채우기 2/knapsack-2.py
+++ 260113/배낭 채우기 2/knapsack-2.py
@@ -1,27 +1,23 @@
-# dp[i][j] 무게가 j 이하일 때 i개의 보석을 골랐을 때 최대 가치
-# j - jewels[i]를 매번 돌기?
-# 100 * 10000 * 100 = 10 ** 8 , 1억번
+# dp[i] 무게가 i일 때 얻을 수 있는 최대 가치
 
 import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().strip().split())
 
-jewels = [0]
+jewels = [(0, 0)]
 for _ in range(n):
     w, v = map(int, input().strip().split())
     jewels.append((w, v))
 
+jewels.sort()
 # print(jewels)
 
-dp = [[0] * (m + 1) for _ in range(n + 1)]
-for i in range(1, n + 1):
-    for j in range(1, m + 1):
-        for k in range(1, len(jewels)):
-            w, v = jewels[k]
-            if j >= w:
-                dp[i][j] = max(dp[i][j], dp[i - 1][j - w] + v)
-            else:
-                dp[i][j] = dp[i - 1][j]
+dp = [0] * (m + 1)
+for i in range(m + 1):
+    for j in range(1, len(jewels)):
+        w, v = jewels[j]
+        if i + w <= m:
+            dp[i + w] = max(dp[i + w], dp[i] + v)
 
-print(dp[n][m])
+print(max(dp))
