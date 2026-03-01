dd88b7548d0c499b710e6b64e18b55de33ac941a
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2026-03-01 22:08:29 +0900

    Title: 정수 두 개의 합 2, Time: 100ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 260301/정수 두 개의 합 2/sum-of-two-integers-2.py
+++ 260301/정수 두 개의 합 2/sum-of-two-integers-2.py
@@ -5,11 +5,9 @@ n, k = map(int, input().split())
 sequence = [int(input()) for _ in range(n)]
 sequence.sort()
 
-total = 0
 count = 0
+j = 1
 for i in range(n - 1):
-    j = i + 1
-    total = sequence[i] + sequence[j]    
     while sequence[i] + sequence[j] <= k:
         count += 1
         j += 1
