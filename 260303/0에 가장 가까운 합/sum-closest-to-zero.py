896529c410bf43b42613744daf502fd12b0f1405
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2026-03-03 21:25:03 +0900

    Title: 0에 가장 가까운 합, Time: 74ms, Memory: 24MB, Status: Wrong Answer - Codetree

---
+++ 260303/0에 가장 가까운 합/sum-closest-to-zero.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+sequence = list(map(int, input().split()))
+sequence.sort()
+
+abs_min = 2e9
+i, j = 0, n - 1
+while i < j:
+    diff = sequence[i] + sequence[j]
+    abs_min = min(abs_min, abs(diff))
+    if abs_min == 0:
+        break
+    if abs_min > 0:
+        j -= 1
+    else:
+        i += 1
+
+answer = abs_min
+print(answer)
