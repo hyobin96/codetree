cee8816eb8009393c7b1ca639ae00e626b44ff6f
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2026-03-07 21:40:42 +0900

    Title: 1이 K개 이상 존재하는 부분 수열, Time: 275ms, Memory: 109MB, Status: Wrong Answer - Codetree

---
+++ 260307/1이 K개 이상 존재하는 부분 수열/subsequence-with-k-or-more-1s.py
@@ -0,0 +1,25 @@
+# k보다 적으면 j++, 많으면 i--
+import sys
+input = sys.stdin.readline
+INF = 2e9
+
+n, k = map(int, input().split())
+sequence = list(map(int, input().split()))
+
+j = 0
+cnt = int(sequence[j] == 1)
+min_length = INF
+
+for i in range(n):
+    
+    while j + 1 < n and cnt < k:
+        cnt += int(sequence[j + 1] == 1)
+        j += 1
+
+    if cnt >= k:
+        min_length = min(min_length, j - i + 1)
+
+    cnt -= int(sequence[i] == 1)
+
+answer = min_length
+print(answer)
