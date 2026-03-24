34275db043c3cbef6f349f367d822ac685b80f56
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2026-03-24 09:08:09 +0900

    Title: 수 채우기, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260324/수 채우기/fill-in-number.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+# Please write your code here.
+cnt = 0
+while n > 0:
+    if (n - 5) % 2 == 0 or n % 5 == 0:
+        n -= 5
+        cnt += 1
+    elif (n - 2) % 5 == 0 or n % 2 == 0:
+        n -= 2
+        cnt += 1
+    else:
+        cnt = -1
+        n = 0
+
+answer = cnt
+print(cnt)
+        
