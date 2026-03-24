5000f82971a85edd8fd0f0cfd13eef932952216c
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2026-03-25 01:06:53 +0900

    Title: 상하좌우 반전시키기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260325/상하좌우 반전시키기/flip-up-down-left-right.py
+++ 260325/상하좌우 반전시키기/flip-up-down-left-right.py
@@ -2,17 +2,21 @@ n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
+def logging():
+    for i in range(n):
+        print(arr[i])
+    print()
+
 in_range = lambda r, c: 0 <= r < n and 0 <= c < n
 drs, dcs = (0, -1, 1, 0), (-1, 0, 0, 1)
 cnt = 0
 for i in range(1, n):
     for j in range(n):
         is_zero_exist = False
-        for d in range(2):
-            nr, nc = i + drs[d], j + dcs[d]
-            if in_range(nr, nc) and arr[nr][nc] == 0:
-                is_zero_exist = True
-                break
+        d = 1
+        nr, nc = i + drs[d], j + dcs[d]
+        if in_range(nr, nc) and arr[nr][nc] == 0:
+            is_zero_exist = True
         if is_zero_exist:
             for dr, dc in zip(drs, dcs):
                 nr, nc = i + dr, j + dc
@@ -20,6 +24,8 @@ for i in range(1, n):
                     arr[nr][nc] ^= 1
             arr[i][j] ^= 1
             cnt += 1
+        # logging()
+
 
 for i in range(n):
     for j in range(n):
