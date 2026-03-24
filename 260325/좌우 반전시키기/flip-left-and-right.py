ad8b896c11408da89feca62bb97136accdca1ace
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2026-03-25 00:36:22 +0900

    Title: 좌우 반전시키기, Time: 77ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 260325/좌우 반전시키기/flip-left-and-right.py
@@ -0,0 +1,15 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+cnt = 0
+for i in range(1, n + 1):
+    if arr[i - 1] == 0:
+        arr[i - 1] ^= 1
+        arr[i] ^= 1
+        if i != n:
+            arr[i + 1] ^= 1
+        cnt += 1
+
+answer = cnt if arr[-1] != 0 else -1
+print(answer)
