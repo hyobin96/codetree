933bac5f3658a0b8419a622881739fa5513a2801
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2026-03-24 09:29:59 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 54ms, Memory: 19MB, Status: Passed - Codetree

--- 260324/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
+++ 260324/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
@@ -2,7 +2,7 @@ n = int(input())
 price = list(map(int, input().split()))
 
 # Please write your code here.
-prev_price = 100_000
+prev_price = 2 ** 31 - 1
 max_profit = 0
 for p in price:
     max_profit = max(max_profit, p - prev_price)
