0fba2489e3146145ee5bcc9a9555e560f105963d
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2026-03-26 22:24:46 +0900

    Title: 트리 노드 제거, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260326/트리 노드 제거/remove-tree-node.py
+++ 260326/트리 노드 제거/remove-tree-node.py
@@ -15,14 +15,14 @@ def count_leaf_node(root, tree, deleted_node):
     leaf_node_count = 0
     while stack:
         p = stack.pop()
-        if len(tree[p]) == 0:
-            leaf_node_count += 1
-            continue
-
+        is_leaf_node = True
         for child in tree[p]:
             if child == deleted_node:
                 continue
             stack.append(child)
+            is_leaf_node = False
+        
+        leaf_node_count += int(is_leaf_node)
     
     return leaf_node_count
 
