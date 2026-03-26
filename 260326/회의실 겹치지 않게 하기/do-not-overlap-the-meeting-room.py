9e3cbfa24dc47fc262cd3063b016ca33487e052d
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2026-03-26 09:09:15 +0900

    Title: 회의실 겹치지 않게 하기, Time: 297ms, Memory: 28MB, Status: Passed - Codetree

--- 260326/회의실 겹치지 않게 하기/do-not-overlap-the-meeting-room.py
+++ 260326/회의실 겹치지 않게 하기/do-not-overlap-the-meeting-room.py
@@ -4,11 +4,11 @@ meetings = [tuple(map(int, input().split(" "))) for _ in range(n)]
 # Please write your code here.
 meetings.sort(lambda m: m[1])
 # print(meetings)
-prev_e, cancle_cnt = 0, 0
-for s, e in meetings:
-    if prev_e > s:
-        cancle_cnt += 1
+prev_end_time, cancle_count = 0, 0
+for start_time, end_time in meetings:
+    if prev_end_time > start_time:
+        cancle_count += 1
         continue
-    prev_e = e
+    prev_end_time = end_time
 
-print(cancle_cnt)
+print(cancle_count)
