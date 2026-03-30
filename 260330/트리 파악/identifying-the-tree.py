eedf16d145e4c9e3d08d7525a135f4113c73ab62
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2026-03-30 10:06:01 +0900

    Title: 트리 파악, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260330/트리 파악/identifying-the-tree.py
+++ 260330/트리 파악/identifying-the-tree.py
@@ -7,25 +7,26 @@ for _ in range(node_count - 1):
     tree[u].append(v)
     tree[v].append(u)
 
-def count_leaf_node(tree, root_node):
-    stack = [root_node]
+def count_leaf_node_depth(tree, root_node):
+    stack = [(root_node, 1)]
     visited = [0] * (len(tree) + 1)
     visited[root_node] = 1
-    leaf_node_count = 0
+    leaf_node_depth = 0
     while stack:
-        node = stack.pop()
+        node, depth = stack.pop()
         
         for nxt_node in list(tree[node]):
             tree[nxt_node].remove(node)
             if len(tree[nxt_node]) == 0:
-                leaf_node_count += 1
+                leaf_node_depth += depth
             if visited[nxt_node]:
                 continue
             visited[nxt_node] = 1
-            stack.append(nxt_node)
+            stack.append((nxt_node, depth + 1))
         
-    return leaf_node_count
+    return leaf_node_depth
     
-leaf_node_count = count_leaf_node(tree, 1)
-answer = int(leaf_node_count % 2 == 1)
+leaf_node_total_depth = count_leaf_node_depth(tree, 1)
+# print(leaf_node_count)
+answer = int(leaf_node_total_depth % 2 == 1)
 print(answer)
