179b578218875d1bcf74d6942df8d9405b6f8313
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2026-04-08 22:21:30 +0900

    Title: 노드의 공통 조상 4, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260408/노드의 공통 조상 4/common-ancestor-of-node-4.py
@@ -0,0 +1,58 @@
+import sys, math
+sys.setrecursionlimit(100_000)
+
+# input
+root = 1
+node_count = int(input())
+edges = [tuple(map(int, input().split())) for _ in range(node_count - 1)]
+query_count = int(input())
+querys = [tuple(map(int, input().split())) for _ in range(query_count)]
+
+H_MAX = int(math.log(node_count, 2)) + 1
+tree = [[] for _ in range(node_count + 1)]
+for u, v in edges:
+    tree[u].append(v)
+    tree[v].append(u)
+
+depths = [0] * (node_count + 1)
+parents = [[0] * (node_count + 1) for _ in range(H_MAX + 1)]
+
+def dfs(curr, prev, tree, depths, parents):
+    for nxt in tree[curr]:
+        if nxt == prev:
+            continue
+        depths[nxt] += depths[curr] + 1
+        parents[0][nxt] = curr
+        dfs(nxt, curr, tree, depths, parents)
+
+dfs(root, 0, tree, depths, parents)
+
+def sparse_table(parents, H_MAX):
+    for h in range(1, H_MAX + 1):
+        for i in range(1, node_count + 1):
+            parents[h][i] = parents[h - 1][parents[h - 1][i]]
+
+sparse_table(parents, H_MAX)
+
+def get_lca(depths, parents, node1, node2):
+    if depths[node1] < depths[node2]:
+        node1, node2 = node2, node1
+
+    for h in range(H_MAX, -1, -1):
+        if depths[node1] - depths[node2] >= (1 << h):
+            node1 = parents[h][node1]
+    
+    if node1 == node2:
+        return node1
+
+    for h in range(H_MAX, -1, -1):
+        if parents[h][node1] != parents[h][node2]:
+            node1 = parents[h][node1]
+            node2 = parents[h][node2]
+    
+    return parents[0][node1]
+
+for node1, node2 in querys:
+    print(get_lca(depths, parents, node1, node2))   
+
+    
