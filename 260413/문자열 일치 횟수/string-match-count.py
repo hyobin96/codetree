80daea30d71c6277c9c0d18e21c1e9694583aff9
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2026-04-13 13:07:42 +0900

    Title: 문자열 일치 횟수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260413/문자열 일치 횟수/string-match-count.py
@@ -0,0 +1,34 @@
+shift_count = int(input().rstrip())
+target = input().rstrip()
+pattern = input().rstrip()
+
+target = '#' + target + target
+pattern = '#' + pattern
+
+t_l = len(target)
+p_l = len(pattern)
+
+f = [0] * p_l
+f[0] = -1
+for i in range(1, p_l):
+    j = f[i - 1]
+    
+    while j >= 0 and pattern[i] != pattern[j + 1]:
+        j = f[j]
+    
+    f[i] = j + 1
+
+j = 0
+count = 0
+for i in range(1, p_l + shift_count - 1):
+    
+    while j >= 0 and pattern[j + 1] != target[i]:
+        j = f[j]
+
+    j += 1
+
+    if j == p_l - 1:
+        count += 1
+        j = f[j]
+
+print(count)
